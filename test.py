be32699e11dae737eeb98a32e2a6b0e48502fe91
sihwan0Lee <stay136@gmail.com>
2020-05-15 14:27:26 +0900

    깃 시험 정리

---
+++ test.py
@@ -0,0 +1,15 @@
+❯ git push -u origin master
+Enumerating objects: 4, done.
+Counting objects: 100% (4/4), done.
+Delta compression using up to 16 threads
+def evennums(num):
+    a = ""
+
+
+    if num % 2 == 0:
+        a = "짝수"
+    else :
+        a = "홀수"
+
+    return a
+
